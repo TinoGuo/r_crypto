49c40302060e2bac8ac743ab9a922acb203ada26
Tino <guochenghaha@gmail.com>
2020-12-28 00:33:46 +0800

    update script

--- publish.sh
+++ publish.sh
@@ -2,7 +2,11 @@
 
 if [ "$1" == 'true' ]; then
   cd rust && make all
-  cd .. && flutter pub publish
+  cd ..
+  echo start copy
+  cp rust/target/x86_64-apple-darwin/release/librcrypto.dylib macos/librcrypto.dylib
+  echo end copy
+  flutter pub publish
 else
   flutter pub publish --dry-run
 fi
